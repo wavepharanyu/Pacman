9d64efa6fa67414a00ef26ec2eb7e6c2a19458e9
wavezzz <pharanyu.s@ku.th>
2017-11-03 16:22:14 +0700

    add key

--- core/src/com/wave/gdx/game/GameScreen.java
+++ core/src/com/wave/gdx/game/GameScreen.java
@@ -1,10 +1,12 @@
 package com.wave.gdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 
 public class GameScreen extends ScreenAdapter {
 	private PacmanGame pacmanGame;
@@ -29,6 +31,11 @@ public class GameScreen extends ScreenAdapter {
         batch.end();
     }
     private void update(float delta) {
-        x += 5;    
+    	if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+            x -= 10;
+        }
+        if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+            x += 10;
+        }   
     }
 }
